7bc1c2ede62f3167654089d5f605902874ef4932
Mercy Akinyemi <mercyakinyemi@Mercys-MacBook-Pro.local>
2025-01-30 23:59:27 -0600

    updated frontend

--- frontend/src/components/RightPanel/ResultSummary.js
+++ frontend/src/components/RightPanel/ResultSummary.js
@@ -9,6 +9,11 @@ const formatEarnings = (earnings) => {
   return earnings < 0 ? `-$${formattedEarnings}` : `$${formattedEarnings}`;
 };
 
+const copySummaryToClipboard = (summary) => {
+  navigator.clipboard.writeText(summary);
+  alert("Summary copied to clipboard")
+}
+
 const ResultSummary = ({
   initialInvestment,
   investmentDuration,
@@ -17,6 +22,16 @@ const ResultSummary = ({
   earnings,
   totalBalance,
 }) => {
+  const summaryText = `
+  Result Summary:
+  Initial Investment: ${formatAmount(initialInvestment)}
+  Investment Duration: ${investmentDuration} years
+  Risk-Free Rate: ${riskFreeRate}%
+  Mutual Fund Beta: ${mutualFundBeta}
+  Earnings: ${formatEarnings(earnings)}
+  --------------------------------
+  Total Balance: ${formatAmount(totalBalance)}
+  `
   return (
     <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
       <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-4">Result Summary</h2>
@@ -54,6 +69,10 @@ const ResultSummary = ({
           <span className="text-xl font-bold text-green-600 dark:text-green-400">{formatAmount(totalBalance)}</span>
         </div>
       </div>
+
+      <button onClick={() => copySummaryToClipboard(summaryText)} className='mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 transition'>
+        Copy
+      </button>
     </div>
   );
 };
